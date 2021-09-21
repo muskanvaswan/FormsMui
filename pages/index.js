85aac0dc64ac669e645e8cd8dc2cf73ad853861b
Muskan Vaswan <muskanvaswan@gmail.com>
2021-09-21 10:32:54 +0530

    change folder structure for components

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
-import Component from '../src/Component'
+import Component from '../src/components/Component'
 
 export default function Index() {
   return (
